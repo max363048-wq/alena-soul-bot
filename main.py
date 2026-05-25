bee58034f9a45593f7ec79e4ae56d2f2b0c5d134
max363048-wq <max363048@gmail.com>
2026-05-25 11:02:39 +0300

    main.py

--- main.py
+++ main.py
@@ -13,24 +13,43 @@ client = OpenAI(
 
 print("Бот запущен...")
 
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет))) Я Алена 💕"
+    )
+
+
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     try:
-        response = client.responses.create(
-            model="gpt-4.1-mini",
-            input=f"""
-Ты милая девушка по имени Алена.
-
-Пользователь: {message.text}
-"""
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Ты милая девушка по имени Алена."
+                },
+                {
+                    "role": "user",
+                    "content": message.text
+                }
+            ]
         )
 
-        reply = response.output_text
+        reply = response.choices[0].message.content
 
-        bot.reply_to(message, reply)
+        bot.send_message(message.chat.id, reply)
 
     except Exception as e:
         print("ERROR:", e)
-        bot.reply_to(message, f"Ошибка: {e}")
+
+        bot.send_message(
+            message.chat.id,
+            f"Ошибка: {e}"
+        )
+
 
 bot.infinity_polling()
